9635fed10cfdda8e3f2fc2cec76a8c24dd16dd90
StartUpWarrior <61089126+GrandFather-Projects@users.noreply.github.com>
2022-09-22 11:15:45 +0530

    AARAMBH - Refactored.

--- src/main/java/com/oms/service/UserManagementService.java
+++ src/main/java/com/oms/service/UserManagementService.java
@@ -1,5 +1,7 @@
 package com.oms.service;
 
+import com.oms.models.UserDetailsEntity;
+import com.oms.models.repository.UserDetailsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,8 +9,25 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserManagementService {
 
+    private final UserDetailsRepository userDetailsRepository;
 
     public String addNewUserDetails() {
+        var newUser = new UserDetailsEntity();
+        newUser.setUserName("OMS");
+        newUser.setUserPass("OMS");
+        newUser.setLoginId("OMS");
+        newUser.setContactNumber("939803163");
+        newUser.setActiveUser(true);
+        var savedUser = userDetailsRepository.save(newUser);
+        return savedUser.getUserName() + " Save with Login Id -" + savedUser.getLoginId() + " User Id generated is :" + savedUser.getId();
+    }
+
+    public String deleteUserDetails() {
+
+        return null;
+    }
+
+    public String updateUserDetails() {
         return null;
     }
 }
--- src/main/java/com/oms/zcontroller/UserManagementController.java
+++ src/main/java/com/oms/zcontroller/UserManagementController.java
@@ -1,26 +1,34 @@
 package com.oms.zcontroller;
 
 import com.oms.service.UserManagementService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserManagementController {
 
     private final UserManagementService userManagementService;
 
-    public UserManagementController(UserManagementService userManagementService) {
-        this.userManagementService = userManagementService;
+    @PostMapping(value = "/addUser")
+    ResponseEntity<String> addNewUser() {
+        return ResponseEntity.ok(userManagementService.addNewUserDetails());
     }
 
-    //@GetMapping(value = "/test")
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    ResponseEntity<String> addNewUser()
-    {
-        userManagementService.addNewUserDetails();
-       return ResponseEntity.ok("WOOW- IT WORKED");
+    @DeleteMapping(value = "/removeUser")
+    ResponseEntity<String> deleteUser() {
+        return ResponseEntity.ok(userManagementService.deleteUserDetails());
     }
+
+    @PutMapping(value = "/updateUser")
+    ResponseEntity<String> updateUser() {
+        return ResponseEntity.ok(userManagementService.updateUserDetails());
+    }
+
 }
