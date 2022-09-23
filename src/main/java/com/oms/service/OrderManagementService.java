4fb4529f1fa886c6b301bd272a7cea805880c37f
StartUpWarrior <61089126+GrandFather-Projects@users.noreply.github.com>
2022-09-24 02:53:47 +0530

    AARAMBH - Implemented excel.

--- src/main/java/com/oms/service/OrderManagementService.java
+++ src/main/java/com/oms/service/OrderManagementService.java
@@ -1,11 +1,21 @@
 package com.oms.service;
 
 import com.oms.mapper.ResponseMapper;
+import com.oms.models.OrderManagerEntity;
 import com.oms.models.repository.CustomerDetailsRepository;
 import com.oms.pojo.CustomerDetailsPojo;
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,4 +41,129 @@ public class OrderManagementService {
         }
 
     }
+
+    public  ByteArrayInputStream test(CustomerDetailsPojo customerDetails) throws IOException {
+var orderManagerEntities=customerDetails.getCustomerOrders();
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("SHEET");
+        int rowIdx=0;
+        Row powerHeaderRowOne=sheet.createRow(rowIdx++);
+        Row powerHeaderRowTwo=sheet.createRow(rowIdx++);
+        sheet.addMergedRegion(new CellRangeAddress(0,1,0,12));
+        sheet.addMergedRegion(new CellRangeAddress(0,1,13,19));
+        powerHeaderRowOne.createCell(0).setCellValue(customerDetails.getCustomerName());
+        powerHeaderRowOne.createCell(13).setCellValue("Electronika Feedback");
+        //sheet.createRow(rowIdx++);
+        XSSFCellStyle style=workbook.createCellStyle();
+
+        CellStyle style2=workbook.createCellStyle();
+        CellStyle style3=workbook.createCellStyle();
+        CellStyle style4=workbook.createCellStyle();
+        CellStyle style5=workbook.createCellStyle();
+
+        var font=workbook.createFont();
+        font.setBold(true);
+        font.setColor(IndexedColors.WHITE.getIndex());
+        font.setFontHeightInPoints((short) 11);
+        style.setFont(font);
+        style2.setFont(font);
+        powerHeaderRowOne.getCell(0).setCellStyle(getCellStyle(1,style));
+        powerHeaderRowOne.getCell(13).setCellStyle(getCellStyle(2,style2));
+        Row headerRow=sheet.createRow(rowIdx++);
+        headerRow.createCell(0).setCellValue("Sr.");
+        headerRow.createCell(1).setCellValue("PO Number");
+        headerRow.createCell(2).setCellValue("Order Date");
+        headerRow.createCell(3).setCellValue("Customer Name");
+        headerRow.createCell(4).setCellValue("Customer Part No");
+        headerRow.createCell(5).setCellValue("Manufacturer Item No");
+        headerRow.createCell(6).setCellValue("Item Description");
+        headerRow.createCell(7).setCellValue("MFG/Make");
+        headerRow.createCell(8).setCellValue("INR Price/pcs");
+        headerRow.createCell(9).setCellValue("PO Qty");
+        headerRow.createCell(10).setCellValue("Customer Requested Date(CRD)");
+        headerRow.createCell(11).setCellValue("Supplied Qty");
+        headerRow.createCell(12).setCellValue("Pending Qty");
+        headerRow.createCell(13).setCellValue("ESPL PO/EBIS No");
+        headerRow.createCell(14).setCellValue("Supplier deliver Date");
+        headerRow.createCell(15).setCellValue("Invoice No");
+        headerRow.createCell(16).setCellValue("End Customer Bill Date");
+        headerRow.createCell(17).setCellValue("POV");
+        headerRow.createCell(18).setCellValue("Remarks");
+        headerRow.createCell(19).setCellValue("PO Image");
+        for (int i = 0; i < 20; i++) {
+            if(i<=13)
+            {
+                headerRow.getCell(i).setCellStyle(getCellStyle(3,style3));
+            }else{
+               headerRow.getCell(i).setCellStyle(getCellStyle(4,style4));
+            }
+
+        }
+        for (OrderManagerEntity order : orderManagerEntities) {
+            Row row = sheet.createRow(rowIdx++);
+            int rowCounter = 1;
+            row.createCell(0).setCellValue(++rowCounter);
+            row.createCell(1).setCellValue(order.getPoNumber().toString());
+            row.createCell(2).setCellValue(order.getPoDate().toString());
+            row.createCell(3).setCellValue("test");//order.getCustomerDetails().getCustomerName()
+            row.createCell(4).setCellValue(order.getCustomerPartNo().toString());
+            row.createCell(5).setCellValue(order.getMfgItemNo());
+            row.createCell(6).setCellValue(order.getItemDetails());
+            row.createCell(7).setCellValue(order.getMaker());
+            row.createCell(8).setCellValue(order.getPrice());
+            row.createCell(9).setCellValue(order.getPoQuantity());
+            row.createCell(10).setCellValue(order.getCustomerRequestedDate().toString());
+            row.createCell(11).setCellValue(order.getSuppliedQty());
+            row.createCell(12).setCellValue(order.getPendingQty());
+            row.createCell(13).setCellValue(order.getESPL_PO_OR_EBIS_NO());
+            row.createCell(14).setCellValue(order.getSupplierDeliveryDate());
+            row.createCell(15).setCellValue(order.getInvoiceNo());
+            row.createCell(16).setCellValue(order.getEndCustomerBillDate().toString());
+            row.createCell(17).setCellValue(order.getPov());
+            row.createCell(18).setCellValue(order.getRemarks());
+            row.createCell(19).setCellValue(order.getPoImageLink());
+            for (int i = 0; i < 20; i++) {
+                row.getCell(i).setCellStyle(getCellStyle(0, style5));
+            }
+        }
+        for (int i = 0; i < 20; i++) {
+            sheet.autoSizeColumn(i);
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        workbook.write(out);
+        return new ByteArrayInputStream(out.toByteArray());
+    }
+
+private CellStyle getCellStyle(int styleNo,CellStyle style) {
+    if (styleNo == 1) {
+        style.setFillForegroundColor(IndexedColors.BLUE.getIndex());
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND );
+
+    } else if (styleNo==2){
+        style.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND );
+    }else if (styleNo==3)
+    {
+        style.setFillForegroundColor(IndexedColors.SKY_BLUE.getIndex());
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND );
+    }else if(styleNo==4)
+    {
+        style.setFillForegroundColor(IndexedColors.LIGHT_GREEN.getIndex());
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND );
+    }
+    style.setVerticalAlignment(VerticalAlignment.CENTER);
+    style.setAlignment(HorizontalAlignment.CENTER);
+
+    style.setBorderBottom(BorderStyle.THIN);
+    style.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+    style.setBorderLeft(BorderStyle.THIN);
+    style.setLeftBorderColor(IndexedColors.BLACK.getIndex());
+    style.setBorderRight(BorderStyle.THIN);
+    style.setRightBorderColor(IndexedColors.BLACK.getIndex());
+    style.setBorderTop(BorderStyle.THIN);
+    style.setTopBorderColor(IndexedColors.BLACK.getIndex());
+    style.setBorderBottom(BorderStyle.THIN);
+    return style;
+}
+
 }
--- src/main/java/com/oms/zcontroller/OrderManagementController.java
+++ src/main/java/com/oms/zcontroller/OrderManagementController.java
@@ -5,6 +5,9 @@ import com.oms.pojo.CustomerDetailsPojo;
 import com.oms.pojo.ResponseStructure;
 import com.oms.service.OrderManagementService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -42,6 +45,25 @@ public class OrderManagementController {
         return ResponseEntity.ok(response);
     }
 
+    @PostMapping(value = "/getExcel",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Resource> getExcel(@RequestBody CustomerDetailsPojo customerDetails) {
+        var response = new ResponseStructure<CustomerDetailsPojo>();
+        String filename = "tutorials.xlsx";
+
+        try{
+            InputStreamResource file = new InputStreamResource(orderManagementService.test(customerDetails));
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
+                    .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
+                    .body(file);
+
+        }catch (Exception e)
+        {
+            response.setError(new OMSError("WENT-WRONG",e.getMessage()));
+        }
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(MediaType.APPLICATION_JSON_VALUE)).body(null);
+    }
 
 
 
